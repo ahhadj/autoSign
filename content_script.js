74c5c9c9dc5f6bb5c3c2bb2419aaf85614312279
ericdj@zju.edu.cn <ericdj@zju.edu.cn>
2018-09-17 22:30:15 +0800

    work for one user

--- content_script.js
+++ content_script.js
@@ -11,7 +11,7 @@ onLoad(function() {onLoad.loaded = true;});
 
 function checkSigned() {
 	var signBtn = document.getElementById('signstar_wrapper');
-	if (!signBtn && signBtn.classList.contains('sign_box_bright_signed')){
+	if (!signBtn || signBtn.classList.contains('sign_box_bright_signed')){
 		return true;
 	}else{
 		return false;
@@ -23,13 +23,15 @@ function clickSignBtn() {
 	if (!checkSigned()){
 		signBtn.firstElementChild.click();
 		window.setTimeout(checkSignStatus, 1000);
+	}else{
+		chrome.runtime.sendMessage({"type":"openNext"}, handleResponse);
 	}
 }
 
 function changeToUserLogin() {
 	var userLoginBtn = document.getElementsByClassName('tang-pass-footerBarULogin pass-link').item(0);
 	userLoginBtn.click();
-	window.setTimeout(loginUser("a374427627", "eL_s9x6t"), 1000);
+	window.setTimeout(function() {chrome.runtime.sendMessage({"type":"login"}, handleResponse)}, 1000);
 }
 
 
@@ -51,6 +53,22 @@ function loginUser(name, passwd) {
 	var loginBtn = document.getElementById('TANGRAM__PSP_11__submit');
 	loginBtn.click();
 }
+
+function handleResponse(responseObj) {
+	if (responseObj.type === "done") {
+		window.alert("all done!");
+	}else if (responseObj.type === "openNextTieBa") {
+		window.location = responseObj.url;
+	}else if (responseObj.type === "login") {
+		loginUser(responseObj.user.name, responseObj.user.passwd);
+	}
+}
+
+
 onLoad(function() {
-	clickSignBtn();
+	if (checkSigned()) {
+		chrome.runtime.sendMessage({"type":"openNext"}, handleResponse);
+	}else{
+		clickSignBtn();
+	}
 });
--- event_script.js
+++ event_script.js
@@ -1,9 +1,10 @@
 
 var interstedBarNames = ["混沌大学", "喜马拉雅", "樊登读书会", "刘润", "超级个体", "得到app"];
-var userInfos = [];
+var userInfos = [{"name":"", "passwd":""}];
 
 var userIndex = 0;
 var tiebaIndex = 0;
+var opened = false;
 
 function openNextBaResponse() {
 	var response;
@@ -17,23 +18,24 @@ function openNextBaResponse() {
 			response = {"type" : "changeUser"};
 		}
 	}else{
-		response = {"type" : "changeTieBa",
+		response = {"type" : "openNextTieBa",
 					"url" : "https://tieba.baidu.com/f?ie=utf-8&kw=" + interstedBarNames[tiebaIndex]};
 	}
+	return response;
 }
 
 function loginInfoResponse() {
-	return {"type" : "userInfo",
+	return {"type" : "login",
 			"user" : userInfos[userIndex]};
 }
-
-//region {calls}
-console.log(consoleGreeting);
 //Show Page-Action using the onMessage event
 chrome.runtime.onMessage.addListener(function (requestMessage, sender, sendResponse) {
 	console.log("requestMessage: " + requestMessage.data);
-	if (requestMessage.type === "open") {
-		chrome.pageAction.show(sender.tab.id);
+	if (requestMessage.type === "openNext") {
+		if (!opened){
+			chrome.pageAction.show(sender.tab.id);
+			opened = true;
+		}
 		sendResponse(openNextBaResponse());
 	}else if (requestMessage.type === "login"){
 		sendResponse(loginInfoResponse());
